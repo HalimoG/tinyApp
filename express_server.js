e102df4fc4df9410b957162f6ec99d268ef197f2
Halimo <halimoghelle@gmail.com>
2019-02-12 21:48:34 +0000

    added /urls.json  route

---
+++ express_server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+var PORT = 8080; // default port 8080
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+    res.json(urlDatabase);
+  });
+  
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});

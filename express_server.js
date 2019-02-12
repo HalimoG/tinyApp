38b2cfc7336509732f30e5801d24ad4a5631fedf
Halimo <halimoghelle@gmail.com>
2019-02-12 23:03:48 +0000

    added /urls/:shortURL route

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
-var PORT = 8080; // default port 8080
+var PORT = 8080;
+app.set("view engine", "ejs");
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -10,11 +11,16 @@ var urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
-app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
+app.get("/urls/:shortURL", (req, res) => {
+    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+    res.render("urls_show", templateVars);
   });
-  
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
